ad04dbbc055a0f62b9ad989c18e00e8aae77ce73
Gilbert <your-email@example.com>
2026-06-24 16:48:13 +0200

    Fix project urls import error

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # include your app URLs (store app)
     path('', include('store.urls')),
 ]
-
-# only for local media (voice files)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
